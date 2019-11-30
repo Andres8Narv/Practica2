b0b58a0a59e09c5b31e8948fd4a64a41a1318c5f
Andres8Narv <58370380+Andres8Narv@users.noreply.github.com>
2019-11-30 13:41:16 -0500

    Update Vector.java

--- src/vector/Vector.java
+++ src/vector/Vector.java
@@ -13,20 +13,18 @@ public class Vector {
 
     public static void main(String arg[]) throws IOException
     {
-        /*creacion del objeto para leer por teclado*/
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        /*ingreso del tamaño de arreglos*/
-        System.out.print("\n Ingrese Numero de Datos a Ingresar : ");
+        System.out.print("\n Ingrese el tamaño del array: ");
         int tam = Integer.parseInt(in.readLine());
-        /*creacion del arreglo*/
+        
         int arr[] = new int[tam];
         System.out.println();
-        /*lectura del arreglo*/
+        
         int j = 0;
         for (int i = 0 ; i < arr.length;i++)
         {
             j+=1;
-            System.out.print("Elemento " + j + " : ");
+            System.out.print("Num: " + j + " : ");
             arr[i] = Integer.parseInt(in.readLine());
         }
         burbuja(arr);
@@ -47,7 +45,7 @@ public class Vector {
         }
         for(int i = 0;i < arreglo.length; i++)
         {
-            System.out.print(arreglo[i]+"\n");
+            System.out.print(arreglo[i]+"->");
         }
     }
     
